b5e0d5e186e83268a25ece937ea9af1c09fb5e16
lcslima45 <lcslima45@gmail.com>
2025-03-18 18:20:06 -0300

    Begin the linear system solver

--- front/matrix_front.js
+++ front/matrix_front.js
@@ -47,3 +47,71 @@ function getMatrix() {
         console.log("Erro ao enviar a matriz", error);
     });
 }
+function generateLinearSystem() {
+    var sizeInput = document.getElementById("sizeInput");
+    var divElement = document.getElementById("matrixContainer");
+    size = parseInt(sizeInput.value);
+    divElement.innerHTML = "";
+    for (var i = 0; i < size; i++) {
+        var row = document.createElement("div");
+        for (var j = 0; j <= size; j++) {
+            var input = document.createElement("input");
+            input.type = "text";
+            if (j != size) {
+                input.id = "".concat(i).concat(j);
+            }
+            else {
+                input.id = "b".concat(i);
+                var addEqual = document.createElement("span");
+                addEqual.textContent = "=";
+                row.appendChild(addEqual);
+            }
+            row.appendChild(input);
+        }
+        divElement.appendChild(row);
+    }
+    var linearSystemButton = document.createElement("input");
+    linearSystemButton.type = "button";
+    linearSystemButton.value = "Solve system";
+    linearSystemButton.addEventListener("click", getLinearSystem);
+    divElement.appendChild(linearSystemButton);
+}
+function getLinearSystem() {
+    var matrix = [];
+    var b = [];
+    for (var i = 0; i < size; i++) {
+        matrix[i] = [];
+        for (var j = 0; j <= size; j++) {
+            if (j != size) {
+                var elmOfMatrix = document.getElementById("".concat(i).concat(j));
+                matrix[i][j] = parseFloat(elmOfMatrix.value);
+            }
+            else {
+                var elmOfB = document.getElementById("b".concat(i));
+                b[i] = parseFloat(elmOfB.value);
+            }
+        }
+    }
+    console.log(matrix);
+    console.log(b);
+    fetch("http://localhost:8080/determinant", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+            matrix: matrix,
+            b: b,
+        }),
+    })
+        .then(function (response) { return response.json(); })
+        .then(function (data) {
+        var responseContainer = document.createElement("div");
+        responseContainer.innerHTML = "<div> Resposta </br> ".concat(JSON.stringify(data), " </br> </div>");
+        var matrixContainer = document.getElementById("matrixContainer");
+        matrixContainer.appendChild(responseContainer);
+    })
+        .catch(function (error) {
+        console.log("Erro ao enviar sistema linear", error);
+    });
+}
--- front/matrix_front.ts
+++ front/matrix_front.ts
@@ -19,7 +19,7 @@ function generateMatrix() {
     divElement.appendChild(row);
   }
 
-  const detButton = document.createElement("input");
+  const detButton: HTMLInputElement = document.createElement("input");
   detButton.value = "Find determinant";
   detButton.type = "button";
   divElement.appendChild(detButton);
@@ -28,7 +28,7 @@ function generateMatrix() {
 
 function getMatrix() {
   let matrix: number[][] = [];
-  for (let i = 0; i < size; i++) {
+  for (let i: number = 0; i < size; i++) {
     matrix[i] = [];
     for (let j = 0; j < size; j++) {
       const elementOfMatrix = document.getElementById(
@@ -58,5 +58,88 @@ function getMatrix() {
     })
     .catch((error) => {
       console.log("Erro ao enviar a matriz", error);
+    });
+}
+
+function generateLinearSystem() {
+  const sizeInput = document.getElementById("sizeInput") as HTMLInputElement;
+  const divElement = document.getElementById(
+    "matrixContainer"
+  ) as HTMLDivElement;
+  size = parseInt(sizeInput.value);
+
+  divElement.innerHTML = "";
+
+  for (let i: number = 0; i < size; i++) {
+    const row = document.createElement("div");
+    for (let j = 0; j <= size; j++) {
+      const input: HTMLInputElement = document.createElement("input");
+      input.type = "text";
+      if (j != size) {
+        input.id = `${i}${j}`;
+      } else {
+        input.id = `b${i}`;
+        const addEqual: HTMLSpanElement = document.createElement("span");
+        addEqual.textContent = "=";
+        row.appendChild(addEqual);
+      }
+      row.appendChild(input);
+    }
+    divElement.appendChild(row);
+  }
+
+  const linearSystemButton: HTMLInputElement = document.createElement("input");
+  linearSystemButton.type = "button";
+  linearSystemButton.value = "Solve system";
+  linearSystemButton.addEventListener("click", getLinearSystem);
+  divElement.appendChild(linearSystemButton);
+}
+
+function getLinearSystem() {
+  let matrix: number[][] = [];
+  let b: number[] = [];
+
+  for (let i: number = 0; i < size; i++) {
+    matrix[i] = [];
+
+    for (let j: number = 0; j <= size; j++) {
+      if (j != size) {
+        const elmOfMatrix = document.getElementById(
+          `${i}${j}`
+        ) as HTMLInputElement;
+        matrix[i][j] = parseFloat(elmOfMatrix.value);
+      } else {
+        const elmOfB = document.getElementById(`b${i}`) as HTMLInputElement;
+        b[i] = parseFloat(elmOfB.value);
+      }
+    }
+  }
+
+  console.log(matrix);
+  console.log(b);
+
+  fetch("http://localhost:8080/determinant", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      matrix: matrix,
+      b: b,
+    }),
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      const responseContainer = document.createElement("div");
+      responseContainer.innerHTML = `<div> Resposta </br> ${JSON.stringify(
+        data
+      )} </br> </div>`;
+      const matrixContainer = document.getElementById(
+        "matrixContainer"
+      ) as HTMLDivElement;
+      matrixContainer.appendChild(responseContainer);
+    })
+    .catch((error) => {
+      console.log("Erro ao enviar sistema linear", error);
     });
 }
